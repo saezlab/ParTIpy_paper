90fefad87df21adb64ffc1a267e8a20ca9681483
Philipp Schaefer <philipp.schaefer98@icloud.com>
2026-01-23 20:53:45 +0100

    update the memory plots

--- code/benchmark_memory/memory_meta.py
+++ code/benchmark_memory/memory_meta.py
@@ -1,9 +1,7 @@
 from pathlib import Path
 
-import numpy as np
 import pandas as pd
 import plotnine as pn
-import matplotlib.pyplot as plt
 
 from ..utils.const import FIGURE_PATH, OUTPUT_PATH
 
@@ -20,7 +18,9 @@ df_0["dataset"] = "k562"
 df_1 = pd.read_csv(Path("output") / "hct116_memory_bench" / "hct116_memory_results.csv")
 df_1["dataset"] = "hct116"
 
-df_2 = pd.read_csv(Path("output") / "hek293t_memory_bench" / "hek293t_memory_results.csv")
+df_2 = pd.read_csv(
+    Path("output") / "hek293t_memory_bench" / "hek293t_memory_results.csv"
+)
 df_2["dataset"] = "hek293t"
 
 df = pd.concat([df_0, df_1, df_2])
@@ -37,26 +37,41 @@ p1 = (
     pn.ggplot(df)
     + pn.geom_point(
         pn.aes(x="n_cells", y="mem_rss_peak_over_start_mb", color="coreset_fraction"),
-        alpha=0.5
+        alpha=0.5,
     )
     + pn.facet_grid(rows="dataset", cols="n_archetypes")
     + pn.scale_x_log10()
     + pn.scale_color_manual(values=coreset_colors, name="Coreset\nFraction")
-    + pn.theme_minimal()
+    + pn.theme_bw()
+    + pn.labs(x="Number of Cells", y="Peak Memory Usage (MB)")
+    + pn.theme(strip_background=pn.element_rect(fill="none", color="none"))
 )
 p1.save(figure_dir / "memory_usage_memory_meta.pdf", dpi=300, width=8, height=6)
 p1.save(figure_dir / "memory_usage_memory_meta.png", dpi=300, width=8, height=6)
 
-p2 = (
+p2 = p1 + pn.scale_y_log10()
+p2.save(figure_dir / "memory_usage_memory_meta_logy.pdf", dpi=300, width=8, height=6)
+p2.save(figure_dir / "memory_usage_memory_meta_logy.png", dpi=300, width=8, height=6)
+
+p3 = (
     pn.ggplot(df)
     + pn.geom_point(
-        pn.aes(x="n_cells", y="time_compute", color="coreset_fraction"),
-        alpha=0.5
+        pn.aes(x="n_cells", y="time_compute", color="coreset_fraction"), alpha=0.5
     )
     + pn.facet_grid(rows="dataset", cols="n_archetypes")
     + pn.scale_x_log10()
     + pn.scale_color_manual(values=coreset_colors, name="Coreset\nFraction")
-    + pn.theme_minimal()
+    + pn.theme_bw()
+    + pn.labs(x="Number of Cells", y="Computation Time (s)")
+    + pn.theme(strip_background=pn.element_rect(fill="none", color="none"))
 )
-p2.save(figure_dir / "computation_time_memory_meta.pdf", dpi=300, width=8, height=6)
-p2.save(figure_dir / "computation_time_memory_meta.png", dpi=300, width=8, height=6)
+p3.save(figure_dir / "computation_time_memory_meta.pdf", dpi=300, width=8, height=6)
+p3.save(figure_dir / "computation_time_memory_meta.png", dpi=300, width=8, height=6)
+
+p4 = p3 + pn.scale_y_log10()
+p4.save(
+    figure_dir / "computation_time_memory_meta_logy.pdf", dpi=300, width=8, height=6
+)
+p4.save(
+    figure_dir / "computation_time_memory_meta_logy.png", dpi=300, width=8, height=6
+)
